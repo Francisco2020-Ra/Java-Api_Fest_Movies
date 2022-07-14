c673e51a929e2f66c1ad18c82de37e24f55002c8
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2022-07-13 21:41:42 -0300

    Adding project Spring boot

---
+++ src/main/java/com/informatorio/festmovies/FestMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.festmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/festmovies/FestMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.festmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
